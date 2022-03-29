8b8cde7b5d74a6753a7e08a8c6b74ef69771106c
Chris Yang <cyang@deltalearns.ca>
2022-03-29 08:27:40 -0700

    update
    deleted old files
    added board example

--- assignment.py
+++ assignment.py
@@ -4,6 +4,24 @@ Tic Tac Toe
 Create a tic tac toe game that is enclosed entirely within a function.  The basic shell has been created, as well as the main block that will be used to execute the class method that runs the game.
 You need to create the methods and class variables that you will use.
 
+One idea would be to create a board like so:
+
+1 | 2 | 3
+---------
+4 | 5 | 6
+---------
+7 | 8 | 9
+
+or you could also use the keypad to help choose positions:
+
+7 | 8 | 9
+---------
+4 | 5 | 6
+---------
+1 | 2 | 3
+
+To enhance your display, you can access unicode symbols
+╩ for example: https://altcodeunicode.com/
 """
 
 class tictactoe:
